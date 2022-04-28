711cc6c70583c5411257d308380c6f0b746f178d
MiniaczQ <MiniaczQ@users.noreply.github.com>
2022-04-28 23:33:26 +0200

    updated gen

--- map_gen.py
+++ map_gen.py
@@ -27,6 +27,7 @@ def clmp(n):
         return n
 
 with open('map.pl', 'w') as file:
+    add = lambda s: file.write(f'assert({s}).\n')
     file.write('% Tiles\n')
     for x in range(size):
         for y in range(size):
@@ -34,18 +35,18 @@ with open('map.pl', 'w') as file:
             if (x, y) in merchants.keys():
                 # Merchants
                 merchant = merchants[(x, y)]
-                file.write(f'tile_type({id}, t_merchant).\n')
+                add(f'tile_type({id}, t_merchant)')
                 for (p, m) in zip(products, merchant[1]):
-                    file.write(f'merchandise({id}, {p}, {m}).\n')
-                file.write(f'name("{merchant[0]}").\n')
+                    add(f'merchandise({id}, {p}, {m})')
+                add(f'name("{merchant[0]}")')
             else:
                 border = x == 0 | x == dsize | y == 0 | y == dsize
                 if border:
                     # Shallow sea
-                    file.write(f'tile_type({id}, t_shallow).\n')
+                    add(f'tile_type({id}, t_shallow)')
                 else:
                     # Deep sea
-                    file.write(f'tile_type({id}, t_deep).\n')
+                    add(f'tile_type({id}, t_deep)')
     
     file.write('% Paths\n')
     for (dx, dy, dir) in ((-1, 0, 'w'), (1, 0, 'e'), (0, -1, 'n'), (0, 1, 's')):
@@ -53,6 +54,6 @@ with open('map.pl', 'w') as file:
             for y in range(clmp(0 - dy), clmp(size - dy) + 1):
                 id1 = f'tile_{x}_{y}'
                 id2 =f'tile_{x + dx}_{y + dy}'
-                file.write(f'adv_path({id1}, {dir}, {id2}).\n')
+                add(f'adv_path({id1}, {dir}, {id2})')
 
 
